69d13042efd5838b3b7d119889bf2b8c861b6da8
JoyDelearna <delearnajoymae@gmail.com>
2025-12-05 18:22:41 +0800

    Added navbar

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
-// Custom JS will go here
+// ---------------Navbar js start here -----------------------------//
+const navLinks = document.querySelectorAll('.nav-underline');
+
+navLinks.forEach(link => {
+  link.addEventListener('click', function (e) {
+    e.preventDefault(); // prevent default link behavior for demo
+
+    // Remove active from all links
+    navLinks.forEach(l => l.classList.remove('active'));
+
+    // Add active to clicked link
+    this.classList.add('active');
+  });
+});
+// ---------------Navbar js end here -----------------------------//
