e849345d2726dddb94c2d8dbd493464b398573f4
JoyDelearna <delearnajoymae@gmail.com>
2025-12-06 14:44:56 +0800

    Initial Commit for footer

--- js/load-components.js
+++ js/load-components.js
@@ -1,7 +1,17 @@
 document.addEventListener("DOMContentLoaded", () => {
+
+    // Load Navbar
     fetch("components/navbar.html")
         .then(response => response.text())
         .then(data => {
             document.getElementById("navbar-container").innerHTML = data;
         });
+
+    // Load Footer
+    fetch("components/footer.html")
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById("footer-container").innerHTML = data;
+        });
+
 });
